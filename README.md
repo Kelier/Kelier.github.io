3965f95a7ed754571f83b21f1584ca263ab2447e
yanjiangnan <yanjiangnan@baidu.com>
2019-09-03 17:22:40 +0800

    Site updated: 2019-09-03 17:22:37

---
+++ README.md
@@ -0,0 +1,29 @@
+#### Personal Blog Website 👨🏻‍🌾
+
+
+
+
+
+> 没有这城市的星星，那照亮这夜空的是什么？
+
+
+
+
+
+##### Latest Activities
+
+
+
+###### [saga组件库](https://weird-saga.netlify.com/)
+
+
+
+###### [2018-summary](https://app.netlify.com/sites/2018summary)
+
+
+
+###### [复古粒子效果](https://retrowave.netlify.com/)
+
+
+
+###### [TimePicker](https://ex-picker.netlify.com/)
---
+++ js/script.js
@@ -0,0 +1,129 @@
+(function ($) {
+  console.log('%c this is my room 🐝','background:#ea6f5a;color:#fff','请开始脱衣服');
+  var app = $('.app-body')
+  var header = $('.header')
+  var banner = document.getElementById('article-banner') || false
+  var about = document.getElementById('about-banner') || false
+  var top = $('.scroll-top')
+  var catalog = $('.catalog-container .toc-main')
+  var isOpen = false
+
+  $(document).ready(function () {
+    NProgress.start()
+    $('#nprogress .bar').css({
+      'background': '#42b983'
+    })
+    $('#nprogress .spinner').hide()
+
+    var fade = {
+      transform: 'translateY(0)',
+      opacity: 1
+    }
+    if (banner) {
+      app.css('transition-delay', '0.15s')
+      $('#article-banner').children().css(fade)
+    }
+    if (about) {
+      $('.author').children().css(fade)
+    }
+    app.css(fade);
+  })
+
+  window.onload = function () {
+    setTimeout(function () {
+      NProgress.done()
+    }, 200)
+  }
+
+  $('.menu').on('click', function () {
+    if (!header.hasClass('fixed-header') || isOpen) {
+      header.toggleClass('fixed-header')
+      isOpen = !isOpen
+    }
+    $('.menu-mask').toggleClass('open')
+  })
+
+  $('#tag-cloud a').on('click', function () {
+    var list = $('.tag-list')
+    var name = $(this).data('name')
+    var maoH = list.find('#' + name).offset().top
+
+    $('html,body').animate({
+      scrollTop: maoH - header.height()
+    }, 500)
+  })
+
+  $('.reward-btn').on('click', function () {
+    $('.money-code').fadeToggle()
+  })
+
+  $('.arrow-down').on('click', function () {
+    $('html, body').animate({
+      scrollTop: banner.offsetHeight - header.height()
+    }, 500)
+  })
+
+  $('.toc-nav a').on('click', function (e) {
+    e.preventDefault()
+    var catalogTarget = e.currentTarget
+    var scrollTarget = $(catalogTarget.getAttribute('href'))
+    var top = scrollTarget.offset().top
+    if (top > 0) {
+      $('html,body').animate({
+        scrollTop: top - 65
+      }, 500)
+    }
+  })
+
+  top.on('click', function () {
+    $('html, body').animate({ scrollTop: 0 }, 600)
+  })
+
+  document.addEventListener('scroll', function () {
+    var scrollTop = document.documentElement.scrollTop || document.body.scrollTop
+    var headerH = header.height()
+    if (banner) {
+      if (scrollTop > headerH) {
+        header.addClass('fixed-header')
+      } else if (scrollTop === 0) {
+        header.removeClass('fixed-header')
+      }
+    }
+    if (scrollTop > 100) {
+      top.addClass('opacity')
+    } else {
+      top.removeClass('opacity')
+    }
+    if (scrollTop > 190) {
+      catalog.addClass('fixed-toc')
+    } else {
+      catalog.removeClass('fixed-toc')
+    }
+  })
+
+  document.onkeydown=function(e){
+    var keyNum = window.event ? e.keyCode : e.which;
+    if (!document.querySelector('.showemoji')) {
+      var div = document.createElement('div');
+      div.className = 'showemoji';
+      var span = document.createElement('div');
+      span.className = 'emoji';
+      var list = ['🐜', '🐑', '🐻', '🦋', '🐈', '🐊', '🐬'
+      , '🐶', '🐘', '🦅', '🐸', '🐟', '🐎', '🐨',
+      , '🦁', '🐒', '🐭', '🦉', '🐙', '🐧', '🐼'
+      , '🐖', '🐌', '🦄', '🐢', '🐦']
+      span.innerHTML = list[Math.floor((Math.random()*list.length))] || '🐲';
+      div.appendChild(span)
+      document.body.appendChild(div);
+    }
+  };
+
+  document.onkeyup=function(e){
+    var keyNum = window.event ? e.keyCode : e.which;
+    var emoji = document.querySelector('.showemoji');
+    if (emoji) {
+      emoji.className = 'hideemoji';
+    }
+  };
+
+})(jQuery)
